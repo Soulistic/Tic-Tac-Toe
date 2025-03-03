298ca85210a2a7846c04aef72110cc2af5e1daba
Md Faiz Alam <90998194+Soulistic@users.noreply.github.com>
2025-03-03 15:34:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,105 +1,96 @@
-const boxes=document.querySelectorAll(".box");
-const gameInfo=document.querySelector(".game-info");
-const newGameBtn=document.querySelector(".btn");
-const ResetGameBtn=document.querySelector(".resetBtn");
+const boxes = document.querySelectorAll(".box");
+const gameInfo = document.querySelector(".game-info");
+const newGameBtn = document.querySelector(".btn");
+const ResetGameBtn = document.querySelector(".resetBtn");
 let currPlayer;
 let gameGrid;
-const winningPos=[
-    [0,1,2], // row 1
-    [3,4,5],
-    [6,7,8],
-    [0,3,6], // column 1
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],// diagonal top left to bottom right
-    [2,4,6]
+let playerMoves;
+
+const winningPos = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
 ];
-function initGame(){
-    currPlayer="X";
-    gameGrid=["","","","","","","","",""];
-    boxes.forEach((box,index)=>{
-        box.innerText="";
-        boxes[index].style.pointerEvents="all";
-        box.classList = `box box${index+1}`;
-    })
+
+function initGame() {
+    currPlayer = "X";
+    gameGrid = ["", "", "", "", "", "", "", "", ""];
+    playerMoves = { X: [], O: [] };
+    
+    boxes.forEach((box, index) => {
+        box.innerText = "";
+        boxes[index].style.pointerEvents = "all";
+        box.classList = `box box${index + 1}`;
+    });
+    
     ResetGameBtn.classList.remove("active");
     newGameBtn.classList.remove("active");
-    gameInfo.innerText=`Current Player-${currPlayer}`;
+    gameInfo.innerText = `Current Player - ${currPlayer}`;
 }
 initGame();
 
-boxes.forEach((box,index)=>{
-    box.addEventListener('click',()=>{
+boxes.forEach((box, index) => {
+    box.addEventListener("click", () => {
         handleClick(index);
-    })
+    });
 });
 
-function handleClick(index){
-    if(gameGrid[index]===""){
-        boxes[index].innerHTML = currPlayer;
+function handleClick(index) {
+    if (gameGrid[index] === "") {
+        if (playerMoves[currPlayer].length === 3) {
+            let removedIndex = playerMoves[currPlayer].shift();
+            gameGrid[removedIndex] = "";
+            boxes[removedIndex].innerText = "";
+        }
+        
+        boxes[index].innerText = currPlayer;
         gameGrid[index] = currPlayer;
-        boxes[index].style.pointerEvents="none"
-        swapTurn();
+        playerMoves[currPlayer].push(index);
+        boxes[index].style.pointerEvents = "none";
+        
         checkGameStatus();
+        swapTurn();
     }
 }
 
-function swapTurn(){
-    if(currPlayer ==='X'){
-        currPlayer = 'O';
-    }
-    else{
-        currPlayer = 'X';
-    }
-    gameInfo.innerText = `Current Player-${currPlayer}`;
+function swapTurn() {
+    currPlayer = currPlayer === "X" ? "O" : "X";
+    gameInfo.innerText = `Current Player - ${currPlayer}`;
+    boxes.forEach((box, index) => {
+        if (gameGrid[index] === "") {
+            box.style.pointerEvents = "all";
+        }
+    });
 }
 
 function checkGameStatus() {
-    let answer = "";
-
+    let winner = "";
+    
     winningPos.forEach((position) => {
-        //all 3 boxes should be non-empty and exactly same in value
-        if( (gameGrid[position[0]] !== "" || gameGrid[position[1]] !== "" || gameGrid[position[2]] !== "") 
-            && (gameGrid[position[0]] === gameGrid[position[1]] ) && (gameGrid[position[1]] === gameGrid[position[2]])) {
-
-                //check if winner is X
-                if(gameGrid[position[0]] === "X") 
-                    answer = "X";
-                else {
-                    answer = "O";
-                } 
-                    
-                //disable pointer events
-                boxes.forEach((box) => {
-                    box.style.pointerEvents = "none";
-                })
-
-                //now we know X/O is a winner
-                boxes[position[0]].classList.add("win");
-                boxes[position[1]].classList.add("win");
-                boxes[position[2]].classList.add("win");
-            }
+        if (
+            gameGrid[position[0]] !== "" &&
+            gameGrid[position[0]] === gameGrid[position[1]] &&
+            gameGrid[position[1]] === gameGrid[position[2]]
+        ) {
+            winner = gameGrid[position[0]];
+            
+            boxes.forEach((box) => {
+                box.style.pointerEvents = "none";
+            });
+            
+            boxes[position[0]].classList.add("win");
+            boxes[position[1]].classList.add("win");
+            boxes[position[2]].classList.add("win");
+        }
     });
-    if(answer !== ""){
-        gameInfo.innerText=`Winner Player-${answer}`
+    
+    if (winner !== "") {
+        gameInfo.innerText = `Winner - ${winner}`;
         ResetGameBtn.classList.remove("active");
         newGameBtn.classList.add("active");
         return;
     }
-    let fillCount = 0;
-    gameGrid.forEach((box)=>{
-        if(box!=="")
-            fillCount++;
-    });
-    if(fillCount ===1){
-        ResetGameBtn.classList.add("active");
-    }
-
-    if(fillCount === 9) {
-        gameInfo.innerText = "Game Tied !";
-        ResetGameBtn.classList.remove("active");
-        newGameBtn.classList.add("active");
-    }
 }
-newGameBtn.addEventListener('click', initGame);
-ResetGameBtn.addEventListener('click',initGame);
+
+newGameBtn.addEventListener("click", initGame);
+ResetGameBtn.addEventListener("click", initGame);
